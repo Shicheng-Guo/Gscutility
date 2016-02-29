841ccddc3f9ab87380a9006ac7824bb09bc9a3e7
Shicheng Guo <Shicheng.Guo@hotmail.com>
2016-02-29 14:47:31 -0800

    merge hapinfo files in different chromosome

---
+++ hapMergeByChrosome.pl
@@ -0,0 +1,29 @@
+
+#!/usr/bin/perl
+
+# merge the haploinfo files created by bam2hapinfo.pl with different chrosome. 
+use strict;
+use Cwd;
+chdir getcwd;
+
+my @file=glob("*hapInfo.txt");
+
+my %sam;
+foreach my $file(@file){
+        my ($sam,undef)=split /\./,$file;
+        $sam{$sam}=$sam;
+}
+
+foreach my $sam(sort keys %sam){
+my @FileTmp=glob("$sam*");
+my $outFile="$sam.hapInfo.txt";
+print "$outFile\n";
+open OUT,">$outFile";
+foreach my $filetmp(@FileTmp){
+        open F,$filetmp;
+        while(<F>){
+                print OUT $_;
+        }
+}
+close OUT;
+}
