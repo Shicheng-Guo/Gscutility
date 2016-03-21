006b55c0c00ed7b162d47f38fec5333d7a7e233a
Shicheng Guo <Shicheng.Guo@hotmail.com>
2016-03-21 15:49:20 -0700

    Update bam2methyfreqPBS.pl

--- bam2methyfreqPBS.pl
+++ bam2methyfreqPBS.pl
@@ -8,19 +8,19 @@
 
 use strict;
 use Cwd;
-my $dir=getcwd;
-die "Usage: perl ../bam2sortbam.pl <./BAM_Directory>\n" if scalar(@ARGV)<1;
+die "Usage: perl $0 BAM_Directory submit\n" if scalar(@ARGV)<2;
+my $dir=shift @ARGV;
 my $submit=shift @ARGV;
-
+chdir $dir;
 my @bamFile=glob("*.bam");
 my $curr_dir = $dir;
 my $queue="glean";
 my $ppn=6;
-my $walltime="6:00:00";
+my $walltime="7:00:00";
 
 foreach my $bamfile(@bamFile){
 my ($sampleid,undef)=split /.bam/,$bamfile;
-my $job_file_name = $bamfile . ".sortbam2methylfreq.job";
+my $job_file_name = $bamfile."bam2mf.job";
 my $status_file = $bamfile.".status";
 mkdir "../methyfreq" if ! -e "../methyfreq";
 print "$sampleid.sort.bam\n";
@@ -31,7 +31,7 @@ print OUT "#PBS -q $queue\n";
 print OUT "#PBS -l nodes=1:ppn=$ppn\n";
 print OUT "#PBS -l walltime=$walltime\n";
 print OUT "#PBS -o ".$bamfile.".log\n";
-print OUT "#PBS -e ".$bamfile.".err\n";
+print OUT "#PBS -e ".$bamfile.".bam2mf.err\n";
 print OUT "#PBS -V\n";
 print OUT "#PBS -M shihcheng.guo\@gmail.com \n";
 print OUT "#PBS -m abe\n";
@@ -41,7 +41,9 @@ my $is_pairedEnd= & is_bamPE("$bamfile");
 print "$bamfile: $is_pairedEnd ...\n";
 print OUT "bismark_methylation_extractor $is_pairedEnd --bedGraph --multicore 3 --ignore 3 --ignore_3prime 3 --ignore_r2 5 --ignore_3prime_r2 5 --gzip --buffer_size 4G --zero_based --comprehensive --output ../methyfreq  $bamfile\n";
 close OUT;
+if($submit eq "submit"){
 system("qsub $job_file_name");
+}
 }
 
 sub is_bamPE($){
@@ -54,4 +56,12 @@ sub is_bamPE($){
   $is_pairedEnd="--single-end"
   }
   return $is_pairedEnd;
+}
+
+
+sub USAGE{
+print "\nUSAGE: Note: It only works for BAMs Aligned By Bismark!\n"
+print "\nUSAGE: $0 Bam_Directory submit\n";
+print "\nUSAGE: $0 ./ submit\n"
+print "\nUSAGE: Job Files will be created in BAM_Directory\n"
 }
