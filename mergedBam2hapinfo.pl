1778c623473a83f81567cff12e5f691c24792eb5
Shicheng Guo <Shicheng.Guo@hotmail.com>
2016-03-29 10:46:45 -0700

    Update mergedBam2hapinfo.pl

--- mergedBam2hapinfo.pl
+++ mergedBam2hapinfo.pl
@@ -1,10 +1,13 @@
 #!/usr/bin/perl -w
+# Update march/29/2016. Accept bismark aligned BAM
+
 use strict;
 my $cpg_position_file="/home/k4zhang/human_g1k_v37/HsGenome19.CpG.positions.txt";
 my $samtools = "samtools";
 my $target_list_file= $ARGV[0];
 my $merged_bam_file = $ARGV[1];
-my $phred=32;
+my $Aligner=$ARGV[2];
+my $phred=33;
 die("USAGE: mergedBam2hapInfo.pl target_list_file merged_bam\n") if(scalar(@ARGV)<2);
 
 my $target_flanking_len = 80;
@@ -62,6 +65,7 @@ while(my $line = <INFILE>){
 }
 close(INFILE);
 
+if($Aligner eq 'bismark'){
 foreach my $target_id (sort keys(%targetTable)){
 	my ($chr,$target_start,$target_end) = split(/[:\-]/, $target_id);
 	my $region_start = $target_start-$target_flanking_len;
@@ -105,11 +109,13 @@ foreach my $target_id (sort keys(%targetTable)){
 			push(@read_fields,"NA");
 		}
 		my $read_id = scalar(@read_fields)>7 ? join("-", @read_fields[0..6]) : join("-", @read_fields[0..4]);	
-		#print "#$read_id\t$CIGAR\t$read_start\t$read_strand\t$seq\t$qual_string\n";
+		#print "#$read_id\t$CIGAR\t$read_start\t$read_strand\t$seq\t$qual_string\n";		
 		foreach my $CpG_position(@CpG_positions){
 			my $offset = $CpG_position-$read_start+1;
 			next if($offset<0 || $offset >= length($seq));	
-			if($read_strand<16){
+            # the situation sometimes should be change dependent on Flag of different alignmentor
+			if($read_strand <16 || $read_strand ==99 ||$read_strand ==147)
+            {
 				my $qual_score = ord(substr($qual_string,$offset,1))-$phred;
 				next if($readHapInfo{$read_id}->{$CpG_position}->{"qual"} && $readHapInfo{$read_id}->{$CpG_position}->{"qual"} > $qual_score);
 				$readHapInfo{$read_id}->{$CpG_position}->{"base"}=substr($seq,$offset,1);
@@ -119,6 +125,7 @@ foreach my $target_id (sort keys(%targetTable)){
 				next if($readHapInfo{$read_id}->{$CpG_position}->{"qual"} && $readHapInfo{$read_id}->{$CpG_position}->{"qual"} > $qual_score);
 				$readHapInfo{$read_id}->{$CpG_position}->{"base"}=$rcTable{substr($seq,$offset+1,1)};			
 				$readHapInfo{$read_id}->{$CpG_position}->{"qual"}=$qual_score;	
+
 			}	
 			#print "$offset\t", $readHapInfo{$read_id}->{$CpG_position}, "\n";
 		}	
@@ -171,5 +178,120 @@ foreach my $target_id (sort keys(%targetTable)){
 		next if($valid_hap =~ /[AG]/);
 		print $target_id,"\t$valid_hap\t", $hapInfoTable{$target_id}->{"hap_counts"}->{$hap_string},"\t",join(",",@valid_positions), "\n";
 	}
+}
+}else{
+foreach my $target_id (sort keys(%targetTable)){
+	my ($chr,$target_start,$target_end) = split(/[:\-]/, $target_id);
+	my $region_start = $target_start-$target_flanking_len;
+	my $region_end = $target_end+$target_flanking_len;
+	my $cmd = "$samtools view -q 10 $merged_bam_file $chr:$region_start-$region_end";
+	my $ans = `$cmd`;
+	my @lines = split(/\n/, $ans);
+	next if(scalar(@lines)<1 || !$targetTable{$target_id}->{"CpG_positions"});
+	$reads_parsed += scalar(@lines);
+	$reads_after_filtering += scalar(@lines);
+	my @CpG_positions = sort {$a <=> $b} @{$targetTable{$target_id}->{"CpG_positions"}};
+	my %readHapInfo;	
+
+	#print "##$cmd\n";
+	#Going through the reads one at a time
+	my %read_start_pos_table;
+	foreach my $line (@lines){
+		my @fields = split(/\t/, $line);
+		my $read_strand = $fields[1];		
+		my $seq = $fields[9];		
+		my $qual_string = $fields[10];
+		my $read_start = $fields[3];
+		my $CIGAR = $fields[5];
+		my $read_length = length($seq);
+		next if($CIGAR =~ /[ID]/);
+		if($CIGAR=~ /S/){
+			my ($clip_len, @others) = split(/S/, $CIGAR);
+			next if(length($clip_len)>2);
+			$seq=substr($seq,$clip_len,$read_length-$clip_len);		
+			$qual_string=substr($qual_string,$clip_len,$read_length-$clip_len);	
+			$read_length -= $clip_len;			
+		}elsif($seq =~ /^CG/){
+			$seq=substr($seq,2,$read_length-2);		
+			$qual_string=substr($qual_string,2,$read_length-2);	
+			$read_start += 2;
+			$read_length -= 2;
+		}
+		my @read_fields = split(/[\:\#\_]+/, $fields[0]);
+		my $N_read_fields = scalar(@read_fields);
+		for(my $i=$N_read_fields; $i<=4; $i++){
+			push(@read_fields,"NA");
+		}
+		my $read_id = scalar(@read_fields)>7 ? join("-", @read_fields[0..6]) : join("-", @read_fields[0..4]);	
+		#print "#$read_id\t$CIGAR\t$read_start\t$read_strand\t$seq\t$qual_string\n";		
+		foreach my $CpG_position(@CpG_positions){
+			my $offset = $CpG_position-$read_start+1;
+			next if($offset<0 || $offset >= length($seq));	
+            # the situation sometimes should be change dependent on Flag of different alignmentor
+			if($read_strand & 0x10)
+            {
+				my $qual_score = ord(substr($qual_string,$offset+1,1))-$phred;
+				next if($readHapInfo{$read_id}->{$CpG_position}->{"qual"} && $readHapInfo{$read_id}->{$CpG_position}->{"qual"} > $qual_score);
+				$readHapInfo{$read_id}->{$CpG_position}->{"base"}=$rcTable{substr($seq,$offset+1,1)};			
+				$readHapInfo{$read_id}->{$CpG_position}->{"qual"}=$qual_score;	
+			}else{
+				my $qual_score = ord(substr($qual_string,$offset,1))-$phred;
+				next if($readHapInfo{$read_id}->{$CpG_position}->{"qual"} && $readHapInfo{$read_id}->{$CpG_position}->{"qual"} > $qual_score);
+				$readHapInfo{$read_id}->{$CpG_position}->{"base"}=substr($seq,$offset,1);
+				$readHapInfo{$read_id}->{$CpG_position}->{"qual"}=$qual_score;
+
+			}	
+			#print "$offset\t", $readHapInfo{$read_id}->{$CpG_position}, "\n";
+		}	
+		$read_start_pos_table{$read_id} = $read_start if(!$read_start_pos_table{$read_id} || $read_start_pos_table{$read_id}<$read_start);
+	}		
+	
+	#Use read start as the UMI to collapse multiple clonal reads.
+	my @read_list = keys(%readHapInfo);		
+	my %unique_read_base_info;
+	foreach my $read_id (@read_list){		
+		my $UMI = $read_start_pos_table{$read_id};
+		foreach my $CpG_position (keys(%{$readHapInfo{$read_id}})){
+			next if(!$readHapInfo{$read_id}->{$CpG_position}->{"base"});
+			#print "$CpG_position\n";
+			#print $readHapInfo{$read_id}->{$CpG_position}->{"base"}, "\n";
+			#print $readHapInfo{$read_id}->{$CpG_position}->{"qual"}, "\n";
+			$unique_read_base_info{$UMI}->{$CpG_position}->{$readHapInfo{$read_id}->{$CpG_position}->{"base"}}+=$readHapInfo{$read_id}->{$CpG_position}->{"qual"};
+		}
+	}
+	
+	#Derive the consensus haplotype string based on multiple clonal reads.
+	foreach my $UMI (keys(%unique_read_base_info)){
+		my $hap_string="";
+		foreach my $CpG_position(@CpG_positions){
+			my $base = "N";
+			my $best_qual = 0;
+			if($unique_read_base_info{$UMI}->{$CpG_position}){
+				foreach my $base_call (keys(%{$unique_read_base_info{$UMI}->{$CpG_position}})){
+					next if($unique_read_base_info{$UMI}->{$CpG_position}->{$base_call} <= $best_qual);
+					$best_qual = $unique_read_base_info{$UMI}->{$CpG_position}->{$base_call};
+					$base = $base_call;
+				}
+			}					
+			$hap_string=$hap_string.$base;
+		}	
+		$hapInfoTable{$target_id}->{"hap_counts"}->{$hap_string}++ if($hap_string);
+	}
+
+	#report haplotype strings only on the positions with valid calls.
+	foreach my $hap_string(keys %{$hapInfoTable{$target_id}->{"hap_counts"}}){
+		my @valid_positions;
+		my $valid_hap="";
+		for(my $i=0; $i<length($hap_string); $i++){
+			my $allele = substr($hap_string,$i,1);
+			next if($allele eq "N");
+			$valid_hap = $valid_hap.$allele;
+			push(@valid_positions,$CpG_positions[$i]);
+		}
+		next if(scalar(@valid_positions)<1);
+		next if($valid_hap =~ /[AG]/);
+		print $target_id,"\t$valid_hap\t", $hapInfoTable{$target_id}->{"hap_counts"}->{$hap_string},"\t",join(",",@valid_positions), "\n";
+	}
+}
 }
 	
