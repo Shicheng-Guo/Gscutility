ed3d2dd89e13468205aa35c75e683f8080599b5b
Shicheng Guo <Shicheng.Guo@hotmail.com>
2016-05-02 16:03:09 -0700

    Update bspCirclePlot.R

--- bspCirclePlot.R
+++ bspCirclePlot.R
@@ -1,13 +1,12 @@
 ###   BSP circle plot base on matrix
-M<-matrix(sample(c(0,1,1,1),1000,replace=T),10,10)
+M<-matrix(sample(c(sample(0,100,replace=T),1),400,replace=T),100,4)
 col=colorRampPalette(c("white", "red"))(20)
 circle=c(1,19)
-plot(x=nrow(M),y=ncol(M),type="n",xlab="",ylab="",xlim=c(0,ncol(M)),ylim=c(0,nrow(M)))
-for(i in 1:nrow(M)){
-  for(j in 1:ncol(M)){
-    points(i,j,col=1,pch=circle[M[i,j]+1],cex=1.5)
+plot(x=nrow(M),y=ncol(M),type="n",xlab="",ylab="",xlim=c(0,ncol(M)+1),ylim=c(0,nrow(M)+1))
+for(i in 1:ncol(M)){
+  for(j in 1:nrow(M)){
+    points(i,j,col=1,pch=circle[M[j,i]+1],cex=1)
     
   }
 }
-
 
