779eefe7d6c01458c797e9f4db584ce43428b028
Shicheng Guo <Shicheng.Guo@hotmail.com>
2016-06-09 18:15:23 -0700

    Update HeatMap.R

--- HeatMap.R
+++ HeatMap.R
@@ -1,10 +1,9 @@
 
-HeatMap<-function(data,phen,plot="heatmap.pdf",cexRow = 0.2,cexCol = 0.9,Colv=T,Rowv=T){
+HeatMap<-function(data,phen,varselect=12000,plot="heatmap.pdf",cexRow = 0.01,cexCol = 1.2,Colv=T,Rowv=T){
 #  subset<-order(unlist(apply(data,1,function(x) sd(x,na.rm=T))),decreasing=T)[1:4000]
   x1<-which(phen==unique(phen)[1])
   x2<-which(phen==unique(phen)[2])
-  
-  subset<-order(unlist(apply(data,1,function(x) t.test(x[x1],x[x2])$p.value)),decreasing=F)[1:4000]  
+  subset<-order(unlist(apply(data,1,function(x) t.test(x[x1],x[x2])$p.value)),decreasing=F)[1:varselect]  
   data<-data[subset,]
   library("gplots")
   colors <- colorpanel(75,"midnightblue","mediumseagreen","yellow") 
@@ -20,7 +19,4 @@ HeatMap<-function(data,phen,plot="heatmap.pdf",cexRow = 0.2,cexCol = 0.9,Colv=T,
   heatmap.2(data,trace="none",cexRow = cexRow,cexCol = cexCol, ColSideColors=ColSideColors,density.info="none",col=colors,Colv=Colv,Rowv=Rowv,keysize=0.9, margins = c(5, 10))
   dev.off()
 }
-
-HeatMap(data=myNorm$beta,phen=c(rep('T',3),rep('C',2),rep('T',7)),plot="heatmap.pdf")
-
-
+HeatMap(data=myNorm$beta,phen=c(rep('T',3),rep('C',2),rep('T',7)),varselect=124000,plot="heatmap.124000.pdf")
