a32db9e559a1e4f1b6bfb3b8722d0c866f21482f
Shicheng Guo <Shicheng.Guo@hotmail.com>
2016-06-10 12:46:35 -0700

    Update manhattan.plot.R

--- manhattan.plot.R
+++ manhattan.plot.R
@@ -1,3 +1,4 @@
+
 library(qqman)
 res <- read.table("MVP_ALL_CvsT_BHadjust.txt", header=TRUE,sep="\t")
 SNP=res$probeID
@@ -6,5 +7,5 @@ BP=res$MAPINFO
 P=res$P.Value
 manhattaninput=data.frame(SNP,CHR,BP,P)
 pdf("manhattan.pdf")
-manhattan(manhattaninput)
+manhattan(manhattaninput,col = c("blue4", "orange3"),ylim = c(0,25),genomewideline=-log10(1e-17),lwd=2, suggestiveline=F)
 dev.off()
