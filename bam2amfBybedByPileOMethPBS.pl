4aa0ab456f9c35eb2f18552ad9356ff71d720761
Shicheng Guo <Shicheng.Guo@hotmail.com>
2016-06-29 18:49:09 -0700

    Update bam2amfBybedByPileOMethPBS.pl

--- bam2amfBybedByPileOMethPBS.pl
+++ bam2amfBybedByPileOMethPBS.pl
@@ -1,37 +1,37 @@
-#!/usr/bin/perl -w
+#!/usr/bin/perl
+
+#bin/perl -w Bam to sort Bam with samtools in TSCC
+# Run the script to the Bam directory
+# Contact: Shicheng Guo
+# Version 1.3
+# Update: 2016-01-19
+
 use strict;
 use Cwd;
-my $cur_dir=getcwd;
-chdir $cur_dir;
+my $dir=getcwd;
+my @file=glob("/home/shg047/oasis/monod/BAM/rename/*.bam");
+mkdir "MF_PileOMeth" if ! -e "MF_PileOMeth";
+my $ppn=1;
+my $walltime="7:00:00";
+my $queue="home-k4zhang"; # hotel, home-k4zhang, glean
 
-my @bed=glob("/home/shg047/oasis/monod/mhb/split/x*");
-my $submit=shift @ARGV;
-foreach my $bed (@bed){
-    my @tmp=split/\//,$bed;
-    my $id=$tmp[-1];
-    next if $id=~/LambdaNEB/i;
-    my $job_file_name = $id . ".job";
-    my $status_file = $id.".status";
-        my $hapInfo_file = $id.".hapInfo.txt";
-        my $curr_dir = `pwd`;
-        open(JOB_FILE, "> $job_file_name") || die("Error in opening file $job_file_name.\n");
-        print JOB_FILE "#!/bin/csh\n";
-        print JOB_FILE "#PBS -N $id\n";
-        print JOB_FILE "#PBS -q glean\n";
-        print JOB_FILE "#PBS -l nodes=1:ppn=1\n";
-        print JOB_FILE "#PBS -l walltime=14:00:00\n";
-        print JOB_FILE "#PBS -o ".$id.".log\n";
-        print JOB_FILE "#PBS -e ".$id.".err\n";
-        print JOB_FILE "#PBS -V\n";
-        print JOB_FILE "#PBS -M shicheng.guo\@gmail.com \n";
-        print JOB_FILE "#PBS -m abe\n";
-        print JOB_FILE "#PBS -A k4zhang-group\n";
-        print JOB_FILE "cd $curr_dir\n";
-        my $cmd = "perl PileOMethByBed.pl $bed $id";
-        print JOB_FILE "$cmd\n";
-        close(JOB_FILE);
-        print "Job file $job_file_name created.\n";
-        if($submit eq 'submit'){
-        system("qsub $job_file_name");
-        }
+foreach my $file (@file){
+        my @tmp=split/\//,$file;
+    my ($sample,undef)=split /.bam/,$tmp[$#tmp];
+    my $job_file_name = $sample.".bam2bedGraph.PileOMeth.job";
+    open(OUT, ">./MF_PileOMeth/$job_file_name") || die("Error in opening file $job_file_name.\n");
+    print OUT "#!/bin/csh\n";
+    print OUT "#PBS -N $job_file_name\n";
+    print OUT "#PBS -q $queue\n";
+    print OUT "#PBS -l nodes=1:ppn=$ppn\n";
+    print OUT "#PBS -l walltime=$walltime\n";
+    print OUT "#PBS -o ".$file.".bam2bedGraph.PileOMeth.log\n";
+    print OUT "#PBS -e ".$file.".bam2bedGraph.PileOMeth.err\n";
+    print OUT "#PBS -V\n";
+    print OUT "#PBS -M shicheng.guo\@gmail.com \n";
+    print OUT "#PBS -m abe\n";
+    print OUT "#PBS -A k4zhang-group\n";
+    print OUT "cd $dir\n";
+    print OUT "PileOMeth extract --keepDupes --keepSingleton --keepDiscordant --mergeContext -p 5 -q 10 --minDepth 5 /home/shg047/oasis/db/hg19/hg19.fa $file -o ./MF_PileOMeth/$sample\n";
+    system("qsub ./MF_PileOMeth/$job_file_name");
 }
