36c27e5495fb15c32af7fd05532bfbcb536c0b87
Shicheng Guo <Shicheng.Guo@hotmail.com>
2016-08-31 10:55:52 -0700

    Update bam2bai.pl

--- bam2bai.pl
+++ bam2bai.pl
@@ -4,7 +4,7 @@
 # Run the script to the Bam directory
 # Contact: Shicheng Guo
 # Version 1.3
-# Update: 2016-02-19
+# Update: 2016-08-19
 use strict;
 use Cwd;
 my $dir=getcwd;
@@ -28,4 +28,23 @@ foreach my $file(@file){
  print OUT "samtools index $file";
  close OUT;
  system("qsub $file.bam2bai.job");
+}
+
+sub USAGE{
+print "\nUSAGE: perl $0  submit\n";
+print '
+--------------------------------------------------------------------------------------------------------------
+This command will index for sorted bam files under PBS system. 
+Example:  perl bam2bai.pl submit
+
+Previous Script: 
+1, Download or Prepare SRA Download Configure  (http://www.ebi.ac.uk/ena/data/view/SRP028600)
+2, perl fastqDownload.pl SamConfig.txt 8 submit   (fastq-dump --split-files --gzip)
+3, trim_galore --phred33 --fastqc --stringency 3 -q 20 --trim1 --length 20 --gzip --clip_R1 2 --three_prime_clip_R1 2 --illumina SRR299055_1.fastq.gz --output_dir ../fastq_trim
+4, bismark_genome_preparation ./     # merge all the fa to one file (mm9.fa or hg19.fa)
+5, bismark --bowtie2 --phred33-quals --fastq -L 30 -N 1 --multicore 2 /home/shg047/oasis/db/mm9 -1 ../fastq_trim/SRR610033_1_val_1.fq.gz -2 ../fastq_trim/SRR610033_2_val_2.fq.gz  -o ../bam	
+--------------------------------------------------------------------------------------------------------------
+';
+
+
 }
