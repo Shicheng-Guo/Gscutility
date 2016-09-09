3e735fb3e5c8c781d5b5c553a90019a5b0900ff8
Shicheng Guo <Shicheng.Guo@hotmail.com>
2016-09-08 23:45:02 -0700

    Create mergeHapinfobySRX.pl

---
+++ mergeHapinfobySRX.pl
@@ -0,0 +1,36 @@
+#!/usr/bin/perl -w
+
+# A perl script to build config file for SRS Bam Merge
+# Contact: Shihcheng.Guo@Gmail.com
+# Version 1.3
+# Go to http://sra.dnanexus.com/studies/SRP028600/samples
+# Select SRS and Click Related RUNS then get the Table as the input
+
+use strict;
+use warnings;
+use Cwd;
+
+my $file=shift @ARGV;
+my %SRA;
+open F,$file;
+while(<F>){
+chomp;
+if(/(SRR\d+)/){
+	my $SRR=$1;
+	if(/(SRX\d+)/){
+		my $SRX=$1;
+		print "$SRR\t$SRX\n";
+		push @{$SRA{$SRX}},$SRR;
+		}
+	}
+}
+
+system("rm ../mergeHapinfo/*hapinfo.txt");
+
+
+foreach my $SRX(sort keys %SRA){
+        foreach my $SRR (@{$SRA{$SRX}}){
+        system("cat $SRR*sort.bam.hapInfo.txt >> ../mergeHapinfo/$SRX.hapinfo.txt");
+        }
+        print "\n";
+}
