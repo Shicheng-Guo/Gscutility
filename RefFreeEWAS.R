f364105718cbf78966dd3b0d0da461b35ab9c332
Shicheng Guo <Shicheng.Guo@hotmail.com>
2016-09-28 15:12:11 -0700

    Create RefFreeEWAS.R

---
+++ RefFreeEWAS.R
@@ -0,0 +1,23 @@
+require(RefFreeEWAS)
+
+tmp1 <- lm(t(Y) ~ pred)
+
+dim3 <- EstDimRMT(cbind(t(coef(tmp1)), t(resid(tmp1))), FALSE)$dim
+
+test <- RefFreeEwasModel(M2B(Y), cbind(1, as.numeric(pred)-1), dim3)
+
+testBoot <- BootRefFreeEwasModel(test, 50)
+
+res <- summary(testBoot)
+
+est <- res[, 2, 1, 'mean']
+
+est.sd <- res[, 2, 1, 'sd']
+
+Q2 <- (est/est.sd)^2
+
+median(Q2, na.rm = TRUE)/qf(0.5, 1, dof)  # Inflation factor 
+
+P0 <- 2*exp(pt(-sqrt(Q2), dof, log.p=T)) # 
+
+qqplot1.pvals(P0)
