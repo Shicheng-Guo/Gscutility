3609aee5165298825e93e04fc60123c4dc9d8230
Shicheng Guo <Shicheng.Guo@hotmail.com>
2016-11-26 03:45:24 -0800

    Update write.bed.R

--- write.bed.R
+++ write.bed.R
@@ -1,4 +1,6 @@
-write.bed<-function(bed,file){
+write.bed<-function(bed,file,extend=0){
+  bed[,2]<-as.numeric(as.character(bed[,2]))-extend
+  bed[,3]<-as.numeric(as.character(bed[,3]))+extend
   if(ncol(bed)==3){
   bed[,4]<-paste(bed[,1],":",bed[,2],"-",bed[,3],sep="")  
   }
