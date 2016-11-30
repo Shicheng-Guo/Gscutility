c1b61d8050177aacbbf5a0a656538125e450bd4d
Shicheng Guo <Shicheng.Guo@hotmail.com>
2016-11-29 21:47:37 -0800

    Update bspCirclePlot.R

--- bspCirclePlot.R
+++ bspCirclePlot.R
@@ -10,3 +10,20 @@ for(i in 1:ncol(M)){
   }
 }
 
+
+
+# example:
+
+M<-matrix(c(rep(c(0,1,0,1),each=6),c(rep(c(1,0,1,0),each=6))),12,4)
+M<-matrix(c(rep(c(0,1,0,1),each=6),c(rep(c(0,1,0,1),each=6))),12,4)
+M<-matrix(c(rep(c(0,1),time=6),rep(c(1,0),time=6),rep(c(0,1),time=6),rep(c(1,0),time=6)),12,4)
+
+col=colorRampPalette(c("white", "black"))(20)
+circle=c(1,19)
+plot(x=nrow(M),y=ncol(M),type="n",xlab="",ylab="",xlim=c(0,ncol(M)+1),ylim=c(0,nrow(M)+1))
+for(i in 1:ncol(M)){
+  for(j in 1:nrow(M)){
+    points(i,j,col=1,pch=circle[M[j,i]+1],cex=1)
+    
+  }
+}
