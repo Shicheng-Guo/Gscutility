1e84bfff9385d8e11642eb3ce40f930cde51d7ad
Shicheng Guo <Shicheng.Guo@hotmail.com>
2016-12-06 11:08:51 -0800

    Create chrosomeCut.pl

---
+++ chrosomeCut.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/perl
+
+# cut chromosome from beginning to end with specified windows
+# Usage: perl $0 chr21 1000
+
+use strict;
+use Cwd;
+chdir getcwd;
+
+my $CHR=shift @ARGV;
+my $STEP=shift @ARGV;
+my $chrLen="/oasis/tscc/scratch/shg047/db/hg19/hg19.chrom.sizes";
+open F,$chrLen || die "cannot open $chrLen\n";
+my %len;
+while(<F>){
+my($chr,$len)=split/\s+/;
+$len{$chr}=$len;
+}
+my $start=1;
+my $end=$len{$CHR};
+while($start<($len{$CHR}-$STEP)){
+$end=$start+$STEP;
+my $id="$CHR:$start-$end";
+print "$CHR\t$start\t$end\t$id\n";
+$start=$start+$STEP+1;
+}
