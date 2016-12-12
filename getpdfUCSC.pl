d64f56cdf8fb023c781ff048bdf9ec475ef0e3b7
Shicheng Guo <Shicheng.Guo@hotmail.com>
2016-12-12 14:00:15 -0800

    Update getpdfUCSC.pl

--- getpdfUCSC.pl
+++ getpdfUCSC.pl
@@ -3,6 +3,9 @@
 to download the pdf format picture from UCSC for the desired region
 we should set up the configurations in the webbrowser at first
 the $hgsid should be the same as the local webbrowser 
+
+Can only be run in desktop since you need input hgsid manually from internet explorer. 
+This hgsid is located in the end of the UCSC browser after "hgsid="
 =cut
 
 use strict;
@@ -14,8 +17,9 @@ my $cookie_jar = HTTP::Cookies->new( file => "cookies.lwp", );
 my $browser = LWP::UserAgent->new;
 # $browser->proxy( 'http', 'http://192.168.217.197:808' );
 $browser->cookie_jar($cookie_jar);
+ 
+my $hgsid = "571488981_yHcP0e2041O3MzMSDFD4zQYC0J1D";  # change it each time when you open your chorsome
 
-my $hgsid = "558820101_oZpSEJR7Rm00klfHhr2Vcoe0AJm2"; 
 my $DIR = 'C:\Users\shicheng\Downloads';       
 chdir $DIR or die "Cannot change to the $DIR:$!";
 my $file = "test.txt";    #the coord_file to be download sequence
@@ -46,7 +50,7 @@ for my $Coord (@Coordination) {
     print "change display mode\n$address\n";
 	my $tempory = $browser->get($address);
 
-	my $addition = 20000; #for additional bps
+	my $addition = 2000; #for additional bps
 	my $coor = $chr . ":" . $start . "-" . $end;
 	getPDF( $coor, "_0K", $ID,$sample1);
 	my ($coor2K) =  $chr . ":" . ( $start - $addition ) . "-" . ( $end + $addition );
@@ -55,6 +59,54 @@ for my $Coord (@Coordination) {
 }
 
 sub getPDF {
+	my $add = shift;
+	my $extension = shift;
+	my $ID = shift;
+	my $sample1 = shift;
+	$address ="http://genome.ucsc.edu/cgi-bin/hgTracks?position=$add&hgsid=$hgsid";
+	print "The Address is:\n\t", $address, "\n";
+	my $content = $browser->get($address);
+
+	$content = $browser->get("http://genome.ucsc.edu/cgi-bin/hgTracks?hgsid=$hgsid&hgt.psOutput=on");
+	my @content = split /\n/, $content->content;
+
+	foreach my $temp (@content) {
+		chomp $temp;
+		next if ( $temp =~ /^\<\// );
+		if ( $temp =~ /(hgt_genome.+?pdf)/ ) {
+			my $pdf_add = "http://genome.ucsc.edu/trash/hgt/" . "$1";
+			print "The location of the pdf is:\n\t$pdf_add\n";
+			my $pdf = $browser->get( $pdf_add, ':content_file' => "$ID$extension.pdf" );#
+		}
+	}
+
+}
+
+sub getPDFWithBed {
+	my $add = shift;
+	my $extension = shift;
+	my $ID = shift;
+	my $sample1 = shift;
+	$address ="http://genome.ucsc.edu/cgi-bin/hgTracks?position=$add&hgsid=$hgsid";
+	print "The Address is:\n\t", $address, "\n";
+	my $content = $browser->get($address);
+
+	$content = $browser->get("http://genome.ucsc.edu/cgi-bin/hgTracks?hgsid=$hgsid&hgt.psOutput=on");
+	my @content = split /\n/, $content->content;
+
+	foreach my $temp (@content) {
+		chomp $temp;
+		next if ( $temp =~ /^\<\// );
+		if ( $temp =~ /(hgt_genome.+?pdf)/ ) {
+			my $pdf_add = "http://genome.ucsc.edu/trash/hgt/" . "$1";
+			print "The location of the pdf is:\n\t$pdf_add\n";
+			my $pdf = $browser->get( $pdf_add, ':content_file' => "$ID$extension.pdf" );#
+		}
+	}
+
+}
+
+sub getPDFWithPCRPRIMER {
 	my $add = shift;
 	my $extension = shift;
 	my $ID = shift;
