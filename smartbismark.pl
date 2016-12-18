6ec427fd8a3e1ae79958faf96386f890a9678ae0
Shicheng Guo <Shicheng.Guo@hotmail.com>
2016-12-17 23:27:24 -0800

    Update smartbismark.pl

--- smartbismark.pl
+++ smartbismark.pl
@@ -4,22 +4,31 @@
 # Good habit to creat parameter table for a project
 # Contact: Shicheng Guo
 # Version 1.3
-# Update: Nov/23/2016
+# Update: 12/17/2016
 
 use strict;
 use Cwd;
 use Getopt::Long;
 
-my $input ="saminfo.txt";
-my $submit='nonsubmit';
-my $genome="hg19";
-my $server="TSCC";
+my ($input,$submit,$genome,$server,$help,$queue,%ppn,%multicore,%walltime);
 
-GetOptions ( "input=s"   => \$input,           # string
-             "submit=s"  => \$submit,          # flag
+## default parameters
+$input ="saminfo.txt";
+$submit='nonsubmit';
+$genome="hg19";
+$server="TSCC";
+
+## parse parameters
+GetOptions ( "input=s"   => \$input,          # string
+             "submit=s"  => \$submit,         # flag
              "genome=s" => \$genome,          # string   
-             "server=s" => \$server)          # flag
+             "server=s" => \$server,          # flag
+             "help" => \$help,                # help
+	     "queue=s" => \$queue,              # queue(hotel,pdafm,condo)
+			 )          
 or die("Error in command line arguments\n");
+
+print @ARGV;
 
 my $dir=getcwd;
 
@@ -48,7 +57,6 @@ mkdir "../bedgraph" if ! -e "../bedgraph";
 mkdir "../sortbam" if ! -e "../sortbam";
 mkdir "../methyfreq" if ! -e "../methyfreq";
 
-
 open F,$input;
 while(<F>){
     chomp;
@@ -68,30 +76,41 @@ while(<F>){
     print "$read[0]\tphred= $phred\n";
     }
     
-    my $nodes=1;
-    my $ppn=8;          
-    my $multicore=3;    # the more the better to avoid exceed the queue walltime limit
-    my $curr_dir = $dir;
-    my $queue="hotel";  # hotel,pdafm,condo
-    my %walltime=(
+    %walltime=(
     "hotel" => "168:00:00",
     "condo" => "8:00:00",
     "pdafm" => "72:00:00",
     );
     
+    %ppn=(
+    hotel => "16",
+    pdafm => "32",
+    glean => "16",
+    condo => "16",
+    );
+
+    %multicore=(
+    hotel => "6",
+    pdafm => "12",
+    glean => "6",
+    condo => "6",
+    );
 	
+	my $nodes=1;
+    my $curr_dir = $dir;
+    
+    
     if(scalar(@read) eq 2){
-   
     my($sample,undef)=split /_1.fastq.gz|_R1.fastq.gz/,$read[0]; 	
     my($sample1,undef)=split /.fastq.gz/,$read[0];
     my($sample2,undef)=split /.fastq.gz/,$read[1];
     my $job_file_name = "$sample.pbs";
     open(OUT, ">$job_file_name") || die("Error in opening file $job_file_name.\n");   
     
-	print OUT "#!/bin/csh\n";
+    print OUT "#!/bin/csh\n";
     print OUT "#PBS -N $sample\n";
     print OUT "#PBS -q $queue\n";  # glean is free
-    print OUT "#PBS -l nodes=$nodes:ppn=$ppn\n";
+    print OUT "#PBS -l nodes=$nodes:ppn=$ppn{$queue}\n";
     print OUT "#PBS -l walltime=$walltime{$queue}\n";
     print OUT "#PBS -o $sample.log\n";
     print OUT "#PBS -e $sample.err\n";
@@ -103,11 +122,11 @@ while(<F>){
     print OUT "cd $curr_dir\n";    
     print OUT "# fastq-dump --split-files --gzip $sample\n";
 	print OUT "trim_galore --paired --phred$phred --fastqc --illumina $sample1\.fastq.gz $sample2\.fastq.gz --output_dir ../fastq_trim\n";
-	print OUT "bismark --bowtie2 --multicore 2 --phred$phred-quals --fastq -L 25 -N 1 $BismarkRefereDb -1 ../fastq_trim/$sample1\_val_1.fq.gz -2 ../fastq_trim/$sample2\_val_2.fq.gz -o ../bam\n";
+	print OUT "bismark --bowtie2 --multicore $multicore{$queue} --phred$phred-quals --fastq -L 25 -N 1 $BismarkRefereDb -1 ../fastq_trim/$sample1\_val_1.fq.gz -2 ../fastq_trim/$sample2\_val_2.fq.gz -o ../bam\n";
 	print OUT "filter_non_conversion --paired ../bam/$sample1\_val_1_bismark_bt2_pe.bam\n";
-	print OUT "samtools sort ../bam/$sample1\_val_1_bismark_bt2_pe.nonCG_filtered.bam -o ../sortbam/$sample\_bismark_bt2_pe.sort.bam\n";
+	print OUT "samtools sort -@ 8  ../bam/$sample1\_val_1_bismark_bt2_pe.nonCG_filtered.bam -o ../sortbam/$sample\_bismark_bt2_pe.sort.bam\n";
 	print OUT "samtools index ../sortbam/$sample\_bismark_bt2_pe.sort.bam\n";
-	print OUT "bismark_methylation_extractor --no_overlap --merge_non_CpG --cutoff 10 --multicore 8 --paired-end --bedGraph --ignore 1 --buffer_size 4G --zero_based --comprehensive --output ../methyfreq  ../bam/$sample1\_val_1_bismark_bt2_pe.nonCG_filtered.bam\n\n";
+	print OUT "bismark_methylation_extractor --no_overlap --merge_non_CpG --cutoff 5 --multicore 8 --paired-end --bedGraph --ignore 1 --buffer_size 4G --zero_based --comprehensive --output ../methyfreq  ../bam/$sample1\_val_1_bismark_bt2_pe.nonCG_filtered.bam\n\n";
     }elsif(scalar(@read) == 1){	
   	my($sample,undef)=split /_1.fastq.gz|_R1.fastq.gz/,$read[0]; 	
     my($sample1,undef)=split /.fastq.gz/,$read[0];
@@ -116,7 +135,7 @@ while(<F>){
     print OUT "#!/bin/csh\n";
     print OUT "#PBS -N $sample\n";
     print OUT "#PBS -q $queue\n";  # glean is free
-    print OUT "#PBS -l nodes=$nodes:ppn=$ppn\n";
+    print OUT "#PBS -l nodes=$nodes:ppn=$ppn{$queue}\n";
     print OUT "#PBS -l walltime=$walltime{$queue}\n";
     print OUT "#PBS -o $sample.log\n";
     print OUT "#PBS -e $sample.err\n";
@@ -124,26 +143,78 @@ while(<F>){
     print OUT "#PBS -M shihcheng.guo\@gmail.com \n";
     print OUT "#PBS -m abe\n";
     print OUT "#PBS -A k4zhang-group\n";
+    
 	print OUT "# fastq-dump --split-files --gzip $sample\n";
 	print OUT "trim_galore --phred$phred --fastqc --illumina $sample1.fastq.gz --output_dir ../fastq_trim\n";
-    print OUT "bismark --bowtie2 --phred$phred-quals --fastq -L 32 -N 1 --multicore 2 $BismarkRefereDb ../fastq_trim/$sample1\_trimmed.fq.gz -o ../bam\n";  
+    print OUT "bismark --bowtie2 --phred$phred-quals --fastq -L 32 -N 1 --multicore $multicore{$queue} $BismarkRefereDb ../fastq_trim/$sample1\_trimmed.fq.gz -o ../bam\n";  
 	print OUT "samtools sort ../bam/$sample\_se.bam -o ../sortbam/$sample.sort.bam\n";
 	print OUT "samtools index ../sortbam/$sample.sort.bam\n";
-	print OUT "bismark_methylation_extractor --single-end --bedGraph --ignore 3 --buffer_size 4G --zero_based --comprehensive --output ../methyfreq  ../bam/$sample\_se.bam";
+	print OUT "bismark_methylation_extractor --single-end --bedGraph --cutoff 5 --ignore 1 --buffer_size 4G --zero_based --comprehensive --output ../methyfreq  ../bam/$sample\_se.bam";
     }
    close(OUT);
 }
 
 
-sub USAGE{
-print "------------------------------------------------------------------------------------\n\n
-USAGE: perl $0 --input saminfo.txt --genome hg19  --server TSCC --submit\n";
+sub process_command_line{
+	my $help;
+	my $version;
+	my $command_line;
+	my $smartbismark_version;
+	my $bisReadMapper;
+	my $bismark_version;
+	
+	unless ($command_line){
+    die "Please respecify command line options\n";
+	}
+    
+    ### HELPFILE
+    if ($help){
+    print_helpfile();
+    exit;
+    }
+    
+    if ($version){
+    print <<"VERSION";
+          Bismark - Bisulfite Mapper and Methylation Caller.
 
-print "saminfo.txt format: \n
-SRR1648425_1.fastq.gz   SRR1648425_2.fastq.gz
-SRR1648426_1.fastq.gz   SRR1648426_2.fastq.gz
-SRR1648427_1.fastq.gz   SRR1648427_2.fastq.gz
--------------------------------------------------------------------\n";
+                       SmartBismark Version: $smartbismark_version
+                          Bismark Version: $bismark_version
+                           
+        Copyright 2010-15 Shicheng Guo, University of California, San Diego
+
+VERSION
+    exit;
+    }
+}
+
+
+sub print_helpfile{
+ 
+print << "HOW_TO";
+
+USAGE: smartbismark --input "saminfo.txt" --genome hg19 --server TSCC --submit submit
+
+
+ARGUMENTS:
+
+Last edited on 15 December 2016.
+
+--input    Single-end or pair-end fastq file list. For single-end fastq, one file in each line.
+           For pair-end fastq files, paired fastq files should be listed in one line with TAB. 
+
+--genome   hg19, hg38, mm9, mm10. The option is used to determined which bismark alignment ref-
+           erence will be chosed. if you have new reference rather than huamn and mouse, please
+           creat the methylation alignment reference advanced and you can contact me to do that. 
+
+--server   TSCC, GM(GenomeMiner). Combined with genome, --server and --genome will provide the 
+           location of the alignment reference for bismark. 
+           
+--submit   Submit pbs job or not. SmartBismark will creat pbs job files for each fastq file and
+           defaulty taken the system is PBS system. if --submit="submit", then PBS job will be 
+           submitted and PBS ID will be printed in the STANDOUT.            
+
+HOW_TO
+
 }
 
 
