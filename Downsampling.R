6a6f7661943020291a9f969130c995a63b9d8f4c
Shicheng Guo <Shicheng.Guo@hotmail.com>
2016-12-19 11:10:46 -0800

    Update Downsampling.R

--- Downsampling.R
+++ Downsampling.R
@@ -3,24 +3,24 @@
 # Data: 12/19/2016
 
 # raw data simulation
-data<-abs(matrix(rnorm(450),150,3))
-idx<-sample(1:4,150,replace=T)
-input<-data.frame(data,idx)
-dim(input)
-head(input)
-prop=0.5
+idx<-sample(1:2,10,replace=T)
+idx
 
 # Function for downsampling
-downsamplingbyclust<-function(data,idx,prop){
-input<-data.frame(data,idx)
-Row<-c()
-for(i in unique(input$idx)){
- rowc<-sample(which(input$idx==i),round(prop*length(which(input$idx==i)))) 
- Row<-c(Row,rowc)
+downsamplingbyclust<-function(idx,number){
+INDEX<-c()
+for(i in unique(idx)){
+ if(length(which(idx==i))>=number){
+   index<-sample(which(idx==i),number) 
+ }else{
+   index<-which(idx==i)
+ }
+  INDEX<-c(INDEX,index)
 }
-return(Row)
+return(INDEX)
 }
 
 # output the downsampling data
-down<-input[downsamplingbyclust(data,idx,0.5),]
-write.table(down,file="dowsample.txt",row.names=F,col.names=T,sep="\t",quote=F)
+position<-downsamplingbyclust(idx,number=5)
+DATA<-data[,position]
+write.table(DATA,file="dowsample.txt",row.names=F,col.names=T,sep="\t",quote=F)
