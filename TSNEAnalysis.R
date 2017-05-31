dde8862e1d27311e3dc5de5dd1490d4aa36e159d
Shicheng Guo <Shicheng.Guo@hotmail.com>
2017-05-30 23:03:47 -0700

    Update TSNEAnalysis.R

--- TSNEAnalysis.R
+++ TSNEAnalysis.R
@@ -1,4 +1,6 @@
 TSNEAnalysis<-function(mydata,phen,prefix="TSNE"){
+  print("Missing Value: NA is not allowed in the input matrix!")
+  print("Please be sure row is individual, column is variable!")
   print("t-sne analysis: N rows (objects) x P columns (variables) [same as PCA], be sure rownames should be unique")
   library("tsne")
   output=paste(prefix,".tsne-1.pdf",sep="")
@@ -10,7 +12,7 @@ TSNEAnalysis<-function(mydata,phen,prefix="TSNE"){
   dev.off()
   
   colnames(tsne_iris)<-c("xtsne","ytsne")
-  output=paste(prefix,".tsne-2.pdf",sep="")
+  output=paste(prefix,".tsne.pdf",sep="")
   pdf(output)
   chart = ggplot(data.frame(tsne_rlt), aes(xtsne,ytsne))+geom_point(size=1,alpha=1,aes(colour = factor(phen1)))+ggtitle("tSNE dimensions colored by digit")
   # change the size and alpha could make great beautful figure
