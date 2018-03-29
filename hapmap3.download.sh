df4910be6932e5341d450ec75ff4cf2eb7e69bd0
Shicheng Guo <Shicheng.Guo@hotmail.com>
2018-03-29 01:50:19 -0500

    Update hapmap3.download.sh

--- hapmap3.download.sh
+++ hapmap3.download.sh
@@ -1,6 +1,8 @@
 # https://www.broadinstitute.org/medical-and-population-genetics/hapmap-3
 # b36= GRCh36 =hg18. hg19 == GRCh37. 
 # each population
+
+cd /home/local/MFLDCLIN/guosa/hpc/db/hapmap3/vcf
 wget https://www.broadinstitute.org/files/shared/mpg/hapmap3/hapmap3_r1_b36_fwd.qc.poly.tar.bz2
 wget https://www.broadinstitute.org/files/shared/mpg/hapmap3/relationships_w_pops_051208.txt
 tar xjvf hapmap3_r1_b36_fwd.qc.poly.tar.bz2
