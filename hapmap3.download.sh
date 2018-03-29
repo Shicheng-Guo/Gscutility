0231e6f055f5897ccc43a3192c7d9d29f3be6a2b
Shicheng Guo <Shicheng.Guo@hotmail.com>
2018-03-29 00:46:30 -0500

    Create hapmap3.download.sh

---
+++ hapmap3.download.sh
@@ -0,0 +1,3 @@
+# https://www.broadinstitute.org/medical-and-population-genetics/hapmap-3
+wget https://www.broadinstitute.org/files/shared/mpg/hapmap3/hapmap3_r1_b36_fwd.qc.poly.tar.bz2
+wget https://www.broadinstitute.org/files/shared/mpg/hapmap3/relationships_w_pops_051208.txt
