ecc2b38c4e9fced157a73a029729859d1695c131
Shicheng Guo <Shicheng.Guo@hotmail.com>
2018-03-29 01:38:50 -0500

    Update hapmap3.download.sh

--- hapmap3.download.sh
+++ hapmap3.download.sh
@@ -3,7 +3,11 @@
 # each population
 wget https://www.broadinstitute.org/files/shared/mpg/hapmap3/hapmap3_r1_b36_fwd.qc.poly.tar.bz2
 wget https://www.broadinstitute.org/files/shared/mpg/hapmap3/relationships_w_pops_051208.txt
+tar xjvf hapmap3_r1_b36_fwd.qc.poly.tar.bz2
+tar xjvf relationships_w_pops_051208.txt
 
 # consensus
 wget https://www.broadinstitute.org/files/shared/mpg/hapmap3/hapmap3_r1_b36_fwd_consensus.qc.poly.recode.ped.bz2
 wget https://www.broadinstitute.org/files/shared/mpg/hapmap3/hapmap3_r1_b36_fwd_consensus.qc.poly.recode.map.bz2
+bzip2 -d hapmap3_r1_b36_fwd_consensus.qc.poly.recode.ped.bz2
+bzip2 -d hapmap3_r1_b36_fwd_consensus.qc.poly.recode.map.bz2
