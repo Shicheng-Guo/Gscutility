d4b6f1992d36aad288052de9154f3375e488630b
Shicheng Guo <Shicheng.Guo@hotmail.com>
2018-07-28 16:37:55 -0500

    Create configure_server_step_by_step.sh

---
+++ configure_server_step_by_step.sh
@@ -0,0 +1,10 @@
+This note is used for a step to mention ourselves to configure a bioinformatics server step by step. 
+
+1. Install Ubuntu, Redhat, Centos
+
+2. install R, Python, Perl and related packages. 
+
+3. Download and install UCSC binary ulities
+
+4. Download and install UCSC binary ulities
+
