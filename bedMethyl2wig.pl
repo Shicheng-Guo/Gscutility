7b16bf37adad6b67f57aea339ce84c4b3f0e7290
Shicheng Guo <Shicheng.Guo@hotmail.com>
2018-08-08 13:57:38 -0500

    Update bedMethyl2wig.pl

--- bedMethyl2wig.pl
+++ bedMethyl2wig.pl
@@ -8,7 +8,7 @@ while(<F>){
         my($chr,$start,undef,undef,undef,undef,undef,undef,undef,$read1,$mf1)=split/\s+/,$F1;
         my(undef,$check,$end,undef,undef,undef,undef,undef,undef,$read2,$mf2)=split/\s+/,$F2;
         my $readsum=$read1+$read2;
-        print "Error,CpG dinucleotide assumption was broken!\n";
+        print "$start\t$check\tError,CpG dinucleotide assumption was broken!\n" if $check !=$start+1;
         next if $readsum<3;
         my $mf=($read1*$mf1/100+$read2*$mf2/100)/$readsum;
         print OUT "$chr\t$start\t$end\t$mf\n";
