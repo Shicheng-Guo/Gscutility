109de6c318270caea8a1af9e69c77c410cc91f09
Shicheng Guo <Shicheng.Guo@hotmail.com>
2018-08-08 12:31:49 -0500

    dd
    dd

--- bash/wig2bigwig.pbs.sh
+++
@@ -1,9 +0,0 @@
-for i in `ls *.bedgraph`
-do
-echo \#PBS -N $i  > $i.job
-echo \#PBS -l nodes=1:ppn=1 >> $i.job
-echo cd /gpfs/home/guosa/hpc/epimarker/bedmethyl >> $i.job
-echo j=\"${i//.bed.bedgraph/}\" >> $i.job
-echo wigToBigWig $i ~/hpc/db/hg38/hg38.chrome.sizes \$j.bw >> $i.job
-# qsub $i.job
-done
