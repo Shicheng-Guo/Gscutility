eeef1c1cdaaffbea591dd945d7421addc2e8efc7
Shicheng Guo <Shicheng.Guo@hotmail.com>
2018-08-16 10:55:20 -0500

    Create bam2sortbam.pbs.sh

---
+++ bam2sortbam.pbs.sh
@@ -0,0 +1,13 @@
+for i in `ls *.bam`
+do
+j=${i/.bam/}
+echo \#PBS -N $i.sort  > $i.job
+echo \#PBS -l nodes=1:ppn=6 >> $i.job
+echo \#PBS -M Guo.shicheng\@marshfieldresearch.org >> $i.job
+echo \#PBS -m abe  >> $i.job
+echo \#PBS -q longq  >> $i.job
+echo cd /gpfs/home/guosa/hpc/nash/bam/pool/bam >> ${i}.job
+echo samtools sort -n $i $j.nsort >> ${i}.job
+echo $j
+qsub $i.job
+done
