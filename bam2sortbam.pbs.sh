fefb419e76cd5e547e7aae66051705505c1f4070
Shicheng Guo <Shicheng.Guo@hotmail.com>
2018-08-16 15:39:11 -0500

    Update bam2sortbam.pbs.sh

--- bam2sortbam.pbs.sh
+++ bam2sortbam.pbs.sh
@@ -6,7 +6,9 @@ echo \#PBS -l nodes=1:ppn=6 >> $i.job
 echo \#PBS -M Guo.shicheng\@marshfieldresearch.org >> $i.job
 echo \#PBS -m abe  >> $i.job
 echo \#PBS -q longq  >> $i.job
+echo cd /gpfs/home/guosa/hpc/nash/bam/pool/bam >> ${i}.job
 echo cd $(pwd) >> ${i}.job
-echo samtools sort -n $i $j.nsort >> ${i}.job
-echo $j
+echo samtools sort -n -@ 6 $i $j.nsort >> ${i}.job
+echo $i.job
+qsub $i.job
 done
