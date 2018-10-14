126689cc8665e8139b083cc19ebeb38ff52aa736
Shicheng Guo <Shicheng.Guo@hotmail.com>
2018-10-13 19:44:49 -0500

    Update rs2bed.pl

--- rs2bed.pl
+++ rs2bed.pl
@@ -1,29 +1,48 @@
 
+#!/usr/bin/perl
 use strict;
 use Cwd;
+use List::Util qw( min max );
 chdir getcwd;
-my $file=shift @ARGV;
+
+my $input=shift @ARGV;
+open F,$input || die "cannot open rs number file: $input\n";
 my %snp;
-open F,$file || die "cannot open $file\n";
 while(<F>){
-my($chr,$start,$end,$rs)=split/\s+/;
-my $cord="$chr:$start-$end";
-$snp{$rs}=$cord;
+next if /Start/i;
+chomp;
+my($rs)=split/\s+/;
+$snp{$rs}=$rs;
 }
 close F;
 
-my @file=glob("gwas*.txt");
-foreach my $file(@file){
-print "$file....\n";
-open F,$file;
-open OUT,">$file.bed";
+my $snpdb1="/home/guosa/hpc/db/hg19/allSNP150.hg19.bed";
+my %db1;
+open F,$snpdb1 || die "cannot open $snpdb1\n";
 while(<F>){
-  chomp;
-   my($snp)=split/\s+/;
-   if($snp{$snp}){
-   my($chr,$start,$end)=split /[:|-]/,$snp{$snp};
-   print OUT "$chr\t$start\t$end\t$snp\n";
-}
+my($chr,$start,$end,$rs)=split/\s+/;
+my $cord="$chr:$start-$end";
+$db1{$rs}=$cord;
 }
 close F;
+
+my $snpdb2="/home/guosa/hpc/db/hg38/allSNP150.hg38.bed";
+my %db2;
+open F,$snpdb2 || die "cannot open $snpdb2\n";
+while(<F>){
+my($chr,$start,$end,$rs)=split/\s+/;
+my $cord="$chr:$start-$end";
+$db2{$rs}=$cord;
 }
+close F;
+
+open OUT1,">$input.hg19.bed";
+open OUT2,">$input.hg38.bed";
+foreach my $rs(sort keys %snp){
+my ($chr1,$start1,$end1)=split/[:-]/,$db1{$snp};
+my ($chr2,$start2,$end2)=split/[:-]/,$db2{$snp};
+print OUT1 	"$chr1\t$start1\t$end1\t$snp\n";
+print OUT2 	"$chr2\t$start2\t$end2\t$snp\n";
+}
+close OUT1;
+close OUT2;
