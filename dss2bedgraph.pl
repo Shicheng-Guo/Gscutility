54f5cdb1b5e89375e681b2c752bb891027fd1a03
Shicheng Guo <Shicheng.Guo@hotmail.com>
2019-03-16 01:18:48 -0500

    Create dss2bedgraph.pl

---
+++ dss2bedgraph.pl
@@ -0,0 +1,21 @@
+
+use strict;
+use Cwd;
+my $dir=getcwd;
+my @file=glob("*OA*.dss");
+my %data;
+foreach my $file (@file){
+open F,$file;
+while(<F>){
+        chomp;
+    my($chr,$pos,$X,$M)=split/\s+/;
+    $data{"$chr:$pos"}{"X"} +=$X;
+    $data{"$chr:$pos"}{"M"} +=$M;
+}
+}
+
+foreach my $pos(sort keys %data){
+        my($chr,$end)=split /\:/,$pos;
+        my $start=$end-1;
+        print "$chr\t$start\t$end\t".$data{$pos}{"M"}/$data{$pos}{"X"}."\n" if $data{$pos}{"X"}>4;
+}
