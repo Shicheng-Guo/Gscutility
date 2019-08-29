2eddc1bdee3da985aee568541f7a09b3d3e86e70
Shicheng Guo <Shicheng.Guo@hotmail.com>
2019-08-29 14:08:42 -0500

    Update add_fake_genotype_vcf.pl

--- add_fake_genotype_vcf.pl
+++ add_fake_genotype_vcf.pl
@@ -1,14 +1,16 @@
+
 use strict;
 open F,"/home/guosa/hpc/db/hg19/dbSNP152.hg19.vcf";
 while(<F>){
 chomp;
 my $line=$_;
 if(/^#CHROM/){
-print "$_\tGSC001\n" if /^#/;
+print "$_\tFORMAT\tGSC001\n" if /^#/;
 }elsif(/^#/){
 print "$_\n" if /^#/;
 }else{
-$line=~s/NC_000001.//i;
-print "$line\t0|1\n";
+$line=~s/NC_(0)+//i;
+$line=~s/\.\d+//i;
+print "$line\tGT\t0|1\n";
 }
 }
