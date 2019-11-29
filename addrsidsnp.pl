a83e1f1bf4af17469bf24119447aacbe2e1a22ee
Shicheng Guo <Shicheng.Guo@hotmail.com>
2019-11-29 16:57:14 -0600

    Update addrsidsnp.pl

--- addrsidsnp.pl
+++ addrsidsnp.pl
@@ -4,9 +4,10 @@
 
 use strict;
 use Cwd;
-my $dbsnp153="/home/guosa/hpc/db/dbSNP153/dbSNP153.hg19.vcf.gz";
+my $dbsnp153="dbSNP153.hg19.vcf";
 my %data;
-open F1,'<:gzip',$dbsnp153;
+#open F1,'<:gzip',$dbsnp153 || die "cannot open $dbsnp153";
+open F1,$dbsnp153 || die "cannot open $dbsnp153";
 while(<F1>){
 next if /^#/;
 my ($chr,$pos,$rs,$ref,$alt)=split/\s+/;
@@ -19,14 +20,15 @@ $data{$id}=$rs;
 my $input=shift @ARGV;
 open F2, $input;
 while(<F2>){
-my @line=split/\s/+
+my @line=split/\s+/;
 my $chr=$line[3];
 my $pos=$line[4];
-my $ref=uc $line[5];
-my $alt=uc $line[6];
+my $ref=uc $line[6];
+my $alt=uc $line[5];
 my $id="$chr-$pos-$ref-$alt";
-if(defined $data{id}){
-$line[1]=$data{$id}
+if(defined $data{$id}){
+$line[1]=$data{$id};
 }
 my $print= join("\t",@line);
+print "$print\n";
 }
