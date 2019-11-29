2c5f2d2e7d63680cf807371785a98db9b82e7c4b
Shicheng Guo <Shicheng.Guo@hotmail.com>
2019-11-29 15:35:34 -0600

    Create addrsidsnp.pl

---
+++ addrsidsnp.pl
@@ -0,0 +1,32 @@
+#!/usr/bin/perl
+# add dbsnp153 to the file according to "chr-pos-ref-alt"
+# contact: Shihcheng.Guo@gmail.com
+
+use strict;
+use Cwd;
+my $dbsnp153="/home/guosa/hpc/db/dbSNP153/dbSNP153.hg19.vcf.gz";
+my %data;
+open F1,'<:gzip',$dbsnp153;
+while(<F1>){
+next if /^#/;
+my ($chr,$pos,$rs,$ref,$alt)=split/\s+/;
+$ref=uc $ref;
+$alt= uc $alt;
+my $id="$chr-$pos-$ref-$alt";
+$data{$id}=$rs;
+}
+
+my $input=shift @ARGV;
+open F2, $input;
+while(<F2>){
+my @line=split/\s/+
+my $chr=$line[3];
+my $pos=$line[4];
+my $ref=uc $line[5];
+my $alt=uc $line[6];
+my $id="$chr-$pos-$ref-$alt";
+if(defined $data{id}){
+$line[1]=$data{$id}
+}
+my $print= join("\t",@line);
+}
