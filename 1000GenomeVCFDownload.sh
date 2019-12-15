b6fe62d026b60676baac97b41294c6d177bb5a05
Shicheng Guo <Shicheng.Guo@hotmail.com>
2019-12-15 00:52:03 -0600

    Update 1000GenomeVCFDownload.sh

--- 1000GenomeVCFDownload.sh
+++ 1000GenomeVCFDownload.sh
@@ -4,7 +4,7 @@ do
 wget http://ftp.1000genomes.ebi.ac.uk/vol1/ftp/release/20130502/ALL.chr$i.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz
 wget http://ftp.1000genomes.ebi.ac.uk/vol1/ftp/release/20130502/ALL.chr$i.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz.tbi
 done 
-xxxxxxxx
+xxxxxxxxx
 xxxxxxxxxxx
 # download part data with perl script 
 perl ./vcf_to_ped_convert.pl -vcf ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/release/20110521/ALL.chr13.phase1_integrated_calls.20101123.snps_indels_svs.genotypes.vcf.gz -sample_panel_file ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/release/20110521/phase1_integrated_calls.20101123.ALL.sample_panel -region 13:32889611-32973805 -population GBR -population FIN
