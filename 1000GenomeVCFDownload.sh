b49d44aebe3baee0fbfee33030f5708f5b1feaea
Shicheng Guo <Shicheng.Guo@hotmail.com>
2020-01-01 00:05:57 -0600

    Update 1000GenomeVCFDownload.sh

--- 1000GenomeVCFDownload.sh
+++ 1000GenomeVCFDownload.sh
@@ -1,6 +1,6 @@
 # Download 1000 genome vcf files
 ##
-
+##
 for i in {1..22} X Y MT
 do
 wget http://ftp.1000genomes.ebi.ac.uk/vol1/ftp/release/20130502/ALL.chr$i.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz
